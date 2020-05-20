8a27372e8b24f94b707721c30f8f3a4d3cb30fc8
Vincent Engel <ve@vesion.io>
2020-05-21 00:07:38 +0200

    Add support for authorization to API base class

--- api.ts
+++ api.ts
@@ -1,15 +1,22 @@
 export abstract class API {
   private readonly baseUrl: string;
+  private readonly authorization: string;
 
-  public constructor(baseUrl: string) {
+  public constructor(baseUrl: string, authorization: string) {
     this.baseUrl = baseUrl;
+    this.authorization = authorization;
   }
 
   protected async fetch(
     path: string,
     init?: RequestInit,
   ): Promise<Response> {
-    const response = await fetch(`${this.baseUrl}${path}`, init);
+    const response = await fetch(`${this.baseUrl}${path}`, {
+      headers: {
+        Authorization: this.authorization,
+        ...(init?.headers || {}),
+      },
+    });
 
     if (response.status > 299) throw Error(`HTTP Error: ${response.status}, ${JSON.stringify(await response.json())}`);
 
